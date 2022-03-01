a14d32865f48918ff634562e4309a87344ac0954
Monirul-98 <monirulsifat@gmail.com>
2022-03-01 11:45:36 +0600

    All details added

--- js/script.js
+++ js/script.js
@@ -55,14 +55,14 @@ const loadDetails = (id) => {
 const getDetails = (features) => {
   const details = document.getElementById("details-card");
   details.innerHTML = `
-    <div class="card mb-3 mx-auto p-3" style="max-width: 740px;" >
+    <div class="card mb-3 mx-auto p-3" style="max-width: 840px;" >
         <div class="row g-0">
-            <div class="col-md-3">
+            <div class="col-md-2">
                 <img src="${
                   features.image
                 }" class="img-fluid rounded-start" alt="...">
             </div>
-            <div class="col-md-3">
+            <div class="col-md-2">
                 <div class="card-body">
                     <h5 class="card-title">${features.name}</h5>
                     <p class="card-text"><small class="text-muted">${
@@ -86,21 +86,28 @@ const getDetails = (features) => {
             </div>
             <div class="col-md-3">
                 <div class="card-body">
-                <h5 class="card-title fw-bolder">Sensors</h5>
-                <span>${features.mainFeatures.sensors[0]}</span>,
-                <span>${features.mainFeatures.sensors[1]}</span>,
-                <span>${features.mainFeatures.sensors[2]}</span>,
-                <span>${features.mainFeatures.sensors[3]}</span>,
-                <span>${features.mainFeatures.sensors[4]}</span>,
-                <span>${features.mainFeatures.sensors[5]}</span>
+                <h5 class="card-title fw-bolder">Others</h5>
+                <p><span class="fw-bold">Bluetooth:</span>${
+                  features.others.Bluetooth
+                }</p>
+                <p><span class="fw-bold">GPS:</span>${features.others.GPS}</p>
+                <p><span class="fw-bold">NFC:</span>${features.others.NFC}</p>
+                <p><span class="fw-bold">Radio:</span>${
+                  features.others.Radio
+                }</p>
+                <p><span class="fw-bold">USB:</span>${features.others.USB}</p>
+                <p><span class="fw-bold">WLAN:</span>${features.others.WLAN}</p>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-2">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text"><small class="text-muted">${
-                      features.releaseDate ? features.releaseDate : "Not found"
-                    }</small></p>
+                    <h5 class="card-title fw-bolder">Sensors</h5>
+                    <span>${features.mainFeatures.sensors[0]}</span>,
+                    <span>${features.mainFeatures.sensors[1]}</span>,
+                    <span>${features.mainFeatures.sensors[2]}</span>,
+                    <span>${features.mainFeatures.sensors[3]}</span>,
+                    <span>${features.mainFeatures.sensors[4]}</span>,
+                    <span>${features.mainFeatures.sensors[5]}</span>
                 </div>
             </div>
         </div>
