2e0653037e93e5605b0cdf99dc3b41d30b417c44
Monirul-98 <monirulsifat@gmail.com>
2022-03-01 11:21:08 +0600

    details main features added

--- js/script.js
+++ js/script.js
@@ -34,10 +34,73 @@ const getMobiles = (phones) => {
         <div class="card-body text-center card-body-style">
             <h5 class="card-title fw-bolder">${phone.phone_name}</h5>
             <h6>${phone.brand}</h6>
-            <button type="button" class="btn btn-outline-dark">Details</button>
+            <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-outline-dark">Details</button>
         </div>
     </div>
       `;
     PhoneCards.appendChild(div);
   });
+};
+
+//Details section
+
+//Fetching details data
+const loadDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => getDetails(data.data));
+};
+
+const getDetails = (features) => {
+  const details = document.getElementById("details-card");
+  details.innerHTML = `
+    <div class="card mb-3 mx-auto p-3" style="max-width: 740px;" >
+        <div class="row g-0">
+            <div class="col-md-3">
+                <img src="${
+                  features.image
+                }" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-3">
+                <div class="card-body">
+                    <h5 class="card-title">${features.name}</h5>
+                    <p class="card-text"><small class="text-muted">${
+                      features.releaseDate ? features.releaseDate : "Not found"
+                    }</small></p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body">
+                    <h5 class="card-title fw-bolder">Main features</h5>
+                    <p><span class="fw-bold">Chip Set:</span>${
+                      features.mainFeatures.chipSet
+                    }</p>
+                    <p><span class="fw-bold">Display Size:</span>${
+                      features.mainFeatures.displaySize
+                    }</p>
+                    <p><span class="fw-bold">Memory:</span>${
+                      features.mainFeatures.memory
+                    }</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><small class="text-muted">${
+                      features.releaseDate ? features.releaseDate : "Not found"
+                    }</small></p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text"><small class="text-muted">${
+                      features.releaseDate ? features.releaseDate : "Not found"
+                    }</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
 };
